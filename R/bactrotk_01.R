0995ac11773a202869853c2d2785e54f3ba9be5b
frareb <francois.rebaudo@ird.fr>
2023-02-08 08:42:20 +0100

    init commit

---
+++ R/bactrotk_01.R
@@ -0,0 +1,13 @@
+
+practices <- data.frame(
+  name = c("a", "b", "c"),
+  description = c("this is a", "this is b", "this is c"),
+  reference = c(NA, NA, NA),
+  effect = c(-2, +2, +1)
+)
+
+
+
+ag00 <- list(
+  
+)
