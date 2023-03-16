22d7fa3c8bd8c32505beaabfb8418caa376c997e
audngm <audrey.ngom@outlook.fr>
2023-03-16 10:54:09 +0100

    modification ligne 64

--- R/bactrotk_04.R
+++ R/bactrotk_04.R
@@ -4,7 +4,7 @@
 # -----------------------------------------------------------------------------
 setwd("./R")
 itk <- read.csv(file = "pratiques23022023.csv", dec = ",", header = TRUE) 
-#ETAPE 0 : FORMULES
+######### ETAPE 0 : FORMULES
 get_RN_score <- function(ressncrs){ #Somme des points requis des cartes jouées
   scorei <- sapply(seq_along(ressncrs), function(i){
     score <- sum(itk[itk$modalite.abr %in% ressncrs[i],]$ressources.ncsr)
@@ -17,37 +17,30 @@ get_itk_score <- function(monTK){ #Somme de l'impact des pratiques sur BD :  si 
   })
   return(sum(scorei))
 }
-#ETAPE 1 : ENTREES
-X1 = 10
+######## ETAPE 1 : ENTREES
 BD = 0.6
+X1 = 10 
 X2 = 15
-ag01 <- list( itk = c("pro_effic"), c(X1M_depart = X1 - X1*BD))
-ag02 <- list(itk = c("dsb","dsb","piege_muscba"), c(X2M_depart = X2 - X2*BD))
+
+ag01 <- list( itk = c("pro_effic"), X1M_depart = X1 - X1*BD)
+ag02 <- list(itk = c("dsb","dsb","piege_muscba"), X2M_depart = X2 - X2*BD)
 n <- list(ag01, ag02)
-#ETAPE 2 : RESULTATS INDIV
-scoreitk <- for (i in 1:length(n)){
-  if (get_RN_score(ressncrs = n[[i]][[1]])>n[[i]][2]) { #Vérification condition points nécessaires 
-    print ("Vous n'avez pas assez de points pour jouer ces cartes, changer de cartes")
-  }else{
-    print(get_itk_score(monTK = n[[i]][[1]]))
-  }
+# -----------------------------------------------------------------------------
+######### ETAPE 2 : RESULTATS INDIV #######
+scoreitk2 <- for (i in 1:length(n)) {
+  ifelse ((get_RN_score(ressncrs = n[[c(i,1)]])>n[[c(i,2)]]),  
+  print ("Vous n'avez pas assez de points pour jouer ces cartes, changer de cartes"),
+  print(get_itk_score(monTK = n[[i]][[1]])))
 }
-# test autre fonction itk pour étape 2 
-#scoreitk2 <- for (i in 1:length(n)){
-  #  if (get_RN_score(ressncrs = n[[c(i,1)]])>n[[c(i,2)]]) { #Vérification condition points nécessaires 
-  #    print ("Vous n'avez pas assez de points pour jouer ces cartes, changer de cartes")
-  #  }else{
-  #    print(get_itk_score(monTK = n[[i]][[1]]))
-  #  }
-  #}scoreindiv <- rep(NA, length(n))
+scoreindiv <- rep(NA, length(n))
 scorefinal_indivtest<- for (i in 1:length(n)){ #Resultat actions individuelles 
-  if (get_itk_score(monTK = n[[i]][[1]])>= (0.6*n[[i]][[2]])) { 
-    scoreindiv[i] <- n[[i]][[2]] + (n[[i]][[2]])* (BD+0.1) 
+  if (get_itk_score(monTK = n[[c(i,1)]])>= (0.6*n[[c(i,2)]])) { 
+    scoreindiv[i] <- n[[c(i,2)]] + (n[[c(i,2)]])* (BD+0.1) 
     print (scoreindiv[i])
     print ("Pratiques vertueuses") #positif
     a <- 1
   }else{
-    scoreindiv[i] <-n[[i]][[2]]
+    scoreindiv[i] <-n[[c(i,2)]]
     print (scoreindiv[i]) #négatif
     print ("Pratiques pas assez efficaces")
     a <- -1
@@ -55,20 +48,20 @@ scorefinal_indivtest<- for (i in 1:length(n)){ #Resultat actions individuelles
   as <- sum(rep(a, times = length(n)))
   print(as)
 }
-#ETAPE 3 : RESULTATS COLLECTIFS
+######### ETAPE 3 : RESULTATS COLLECTIFS
 for (i in 1:length(n)) {
   if (as>=0) {
     print("Bravo, la mouche a diminué de 10% dans le paysage")
-    n[[i]][[2]] <- scoreindiv[i]
-    BD <- BD-0.1
+    n[[c(i,2)]] <- scoreindiv[i]
+    BD <- BD-0.05 #réfléchir comment la mouche diminue d'un tour à l'autre #module de dispersion
   }else{
-    print ("Mince...la plupart des joueurs n'ont pas fait les meilleurs choix, la mouche est toujours responsable de ")
-    n[[i]][[2]] <- n[[i]][[2]]
+    print ("Mince...la plupart des joueurs n'ont pas fait les meilleurs choix ")
+    n[[c(i,2)]] <- n[[c(i,2)]]
   }
 }
-#ETAPE 4 : NOUVEAUX  CHOIX DE CARTE POUR PARTIE SUIVANTE
-n[[1]][[1]] <- c("dsb", "dsb")
-n[[2]][[2]] <- c("dsb","dsb", "dsb")
+######### ETAPE 4 : NOUVEAUX  CHOIX DE CARTE POUR PARTIE SUIVANTE
+n[[1]][[1]] <- c("lb_nid", "lb_nid","lb_nid")
+n[[2]][[1]] <- c("dsb","lb_nid", "lb_nid","lb_nid")
 n
-#ETAPE 5 : retour ETAPE 2
+######### ETAPE 5 : retour ETAPE 2
 
