db145f7d03400622df7e04a18dc89783ddc7d3de
frareb <francois.rebaudo@ird.fr>
2023-03-17 12:57:31 +0100

    problème sur l'algo pour calculer si les pratiques sont bonnes ou pas (ou alors je n'ai pas compris la variable rdtOptimal)

--- R/bactrotk_04.R
+++ R/bactrotk_04.R
@@ -31,16 +31,15 @@ bloc01 <- {
 
 # Chargement de la liste des pratiques culturales
 itk <- read.csv(file = "pratiques23022023.csv", dec = ",", header = TRUE) 
-
-# Fonctions
+# la meilleure note technique en utilisant toutes les pratiques : 
+bestITKnote <- sum(itk$impactbdsurdt[itk$impactbdsurdt > 0])
 
 # Somme des points requis des cartes jouées
 get_RN_score <- function(ressncrs){ 
   scorei <- sapply(seq_along(ressncrs), function(i){
     score <- itk[itk$modalite.abr == ressncrs[i],]$ressources.ncsr
-    if(length(score)==0){score <- NA}
+    if(length(score) == 0){score <- NA}
     return(score)
-    # score <- sum(itk[itk$modalite.abr %in% ressncrs[i],]$ressources.ncsr)
   })
   return(sum(scorei))
 }
@@ -49,7 +48,9 @@ get_RN_score <- function(ressncrs){
 #   choix de la pratique efficace contre prévalence mouche
 get_itk_score <- function(monTK){ 
   scorei <- sapply(seq_along(monTK), function(i){
-    score <- sum(itk[itk$modalite.abr %in% monTK[i],]$impactbdsurdt)
+    score <- itk[itk$modalite.abr == monTK[i],]$impactbdsurdt
+    if(length(score) == 0){score <- NA}
+    return(score)
   })
   return(sum(scorei))
 }
@@ -78,9 +79,22 @@ verifNumPoints <- function(listAgriITKetX){
 # --- 3. BOUCLE DU JEU --------------------------------------------------------
 cat(verifNumPoints(listAgriITKetX)) # vérif nombre de points
 
+
+
+
 scoreindiv <- rep(NA, length(listAgriITKetX))
 a <- rep(NA, length(listAgriITKetX))
 scorefinal_indivtest<- for (i in 1:length(listAgriITKetX)) {  #Resultat actions individuelles 
+  # ici tu compares un rendement théorique (tauxDePertesBD*rdtOptimal[i]) et 
+  # une note basée sur les pratiques agricoles. Pour moi ce n'est pas 
+  # comparable car pas dans les mêmes unités. Par exemple un agri1 peut avoir
+  # un rdt optimal de 100 et il n'arrivera jamais à une note supérieure à 100 ?
+  # Et un agri2 avec un rdt optimal de 2 arrivera toujours avec une note 
+  # supérieure à 2 et donc verra son taux de perte amoindri alors qu'il peut
+  # avoir des pratiques très mauvaises par rapport à l'agri1. Le calcul doit 
+  # être indépendant du rdtOptimal.
+  # il faudrait que tu calcules une note technique qui servent à ajuster le 
+  # tauxDePertesBD. J'ai mis un exemple sous la fonction.
   if (get_itk_score(monTK = listAgriITKetX[[c(i,1)]])>= (tauxDePertesBD*rdtOptimal[i])) { # modifier 0.6*n[[c(i,2)]] par rdtOptimal[i]*tauxDePertesBD ? 
     scoreindiv[i] <- rdtOptimal[i] - (rdtOptimal[i])* (tauxDePertesBD-0.1) 
     print (scoreindiv[i])
@@ -96,6 +110,19 @@ scorefinal_indivtest<- for (i in 1:length(listAgriITKetX)) {  #Resultat actions 
   }
   as <- sum(a) #sum(rep(a[i], times = length(listAgriITKetX)))
 } 
+
+# la meilleure note technique en utilisant toutes les pratiques : 
+bestITKnote <- sum(itk$impactbdsurdt[itk$impactbdsurdt > 0])
+# la note d'un agriculteur
+myITKnote <- get_itk_score(monTK = listAgriITKetX[[c(i,1)]])
+# le rapport entre la note agri et la meilleure note
+tauxMyITKnote <- myITKnote/bestITKnote
+# rdt individuel si rien n'est fait
+rdtOptimal[i] - rdtOptimal[i]*tauxDePertesBD
+# rdt individuel avec les pratiques
+rdtOptimal[i] - rdtOptimal[i]*(tauxDePertesBD - tauxDePertesBD*tauxMyITKnote)
+
+
 ######### ETAPE 3 : RESULTATS COLLECTIFS
 for (i in 1:length(listAgriITKetX)) {
   if (as>=0) {
