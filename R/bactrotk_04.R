df4cae9d819e12a620ff17aa020235ed817ebc01
audngm <audrey.ngom@outlook.fr>
2023-04-03 10:30:18 +0200

    Ajour nouveau tableau > Modification abréviation modalités

--- R/bactrotk_04.R
+++ R/bactrotk_04.R
@@ -5,26 +5,29 @@
 setwd("./R")
 
 # --- 1. INITIALISATION DU JEU ------------------------------------------------
-niveauDeDifficulte <- 2
-tauxDePertesBD <- 0.4
-rdtOptimal <- c(30, 30, 30, 30)
-ag01 <- list(
-  itk = c("irr","ferti", "taille","labour", "lb_nid", "pro_effic","lb_fopius"), 
-  X1_depart = rdtOptimal[1] - rdtOptimal[1]*tauxDePertesBD
-)
-ag02 <- list(
-  itk = c("irr","piege_mala","appat_gf", "dsb","insect"), 
-  X2_depart = rdtOptimal[2] - rdtOptimal[2]*tauxDePertesBD
-)
-ag03 <- list(
-  itk = c("insect","piege_mala","appat_gf","ferti","taille","dsb"), 
-  X3_depart = rdtOptimal[3] - rdtOptimal[3]*tauxDePertesBD
-)
-ag04 <- list(
-  itk = c("labour","dsb", "irr"), 
-  X4_depart = rdtOptimal[4] - rdtOptimal[4]*tauxDePertesBD
-)
-listAgriITKetX <- list(ag01, ag02, ag03, ag04)
+bloc00 <- {
+  niveauDeDifficulte <- 4
+  tauxDePertesBD <- 0.1
+  rdtOptimal <- c(20, 20, 20, 20)
+  ag01 <- list(
+    itk = c("pre", "pma"), 
+    X1_depart = rdtOptimal[1] - rdtOptimal[1]*tauxDePertesBD
+  )
+  ag02 <- list(
+    itk = c("irr","pba"), 
+    X2_depart = rdtOptimal[2] - rdtOptimal[2]*tauxDePertesBD
+  )
+  ag03 <- list(
+    itk = c("pba","bio"), 
+    X3_depart = rdtOptimal[3] - rdtOptimal[3]*tauxDePertesBD
+  )
+  ag04 <- list(
+    itk = c("lbn","dsb"), 
+    X4_depart = rdtOptimal[4] - rdtOptimal[4]*tauxDePertesBD
+  )
+  listAgriITKetX <- list(ag01, ag02, ag03, ag04)
+  listAgriITKetX #afficher points
+}
 # -----------------------------------------------------------------------------
 
 # --- 2. CHARGEMENT DES DONNEES ET FONCTIONS ----------------------------------
@@ -111,11 +114,11 @@ calculRdt <- function(){
       ": rdt optimal: ", round(rdtOptimal[i], digits = 2),
       " ; rdt début tour: ", round(listAgriITKetX[[c(i,2)]], digits = 2),
       " ; note ITK: ", round(calculTxNoteITK()[i], digits = 2), 
-      " ; rdt réel: ", round(rdtReel, digits = 2),
+      " ; rdt réel: ", round(floor(rdtReel), digits = 2),
       "\n"
     )
     cat(msg)
-    return(rdtReel)
+    return(floor(rdtReel))
   })
   return(list(tauxDePertesBD, rdtReel))
 }
@@ -139,10 +142,10 @@ listAgriITKetX <- lapply(seq_along(listAgriITKetX), function(i){
   listAgriITKetX[[i]][[2]] <- tourDeJeu[[2]][i]
   return(listAgriITKetX[[i]])
 })
-listAgriITKetX[[1]][[1]] <- c("ferti","lb_nid","taille","dsb")
-listAgriITKetX[[2]][[1]] <- c( "taille","ferti","lb_nid","dsb")
-listAgriITKetX[[3]][[1]] <- c("appat_gf","ferti","lb_nid","dsb")
-listAgriITKetX[[4]][[1]] <- c(  "lb_nid")
+listAgriITKetX[[1]][[1]] <- c("pro_coin","pro_coin", "appat_gf","piege_mala", "lb_nid","taille","appat_gf")
+listAgriITKetX[[2]][[1]] <- c( "pro_coin","pro_coin", "appat_gf","piege_mala", "lb_nid","taille","appat_gf")
+listAgriITKetX[[3]][[1]] <- c("pro_coin","pro_coin", "appat_gf","piege_mala", "lb_nid","taille","appat_gf")
+listAgriITKetX[[4]][[1]] <- c( "pro_coin","pro_coin", "appat_gf","piege_mala", "lb_nid","taille","appat_gf")
 
 # --- 4. CHANGEMENTS ANNEXES AU COURS DU JEU --------------------------------------------------------
 niveauDeDifficulte <- 2 #nv1 : les agri doivent faire beaucoup de pratiques
@@ -150,35 +153,7 @@ niveauDeDifficulte <- 2 #nv1 : les agri doivent faire beaucoup de pratiques
 #nv3 : les agri doivent faire au moins un tiers des pratiques
 
 
-# scoreindiv <- rep(NA, length(listAgriITKetX))
-# a <- rep(NA, length(listAgriITKetX))
-# scorefinal_indivtest<- for (i in 1:length(listAgriITKetX)) {  #Resultat actions individuelles 
-#   # ici tu compares un rendement théorique (tauxDePertesBD*rdtOptimal[i]) et 
-#   # une note basée sur les pratiques agricoles. Pour moi ce n'est pas 
-#   # comparable car pas dans les mêmes unités. Par exemple un agri1 peut avoir
-#   # un rdt optimal de 100 et il n'arrivera jamais à une note supérieure à 100 ?
-#   # Et un agri2 avec un rdt optimal de 2 arrivera toujours avec une note 
-#   # supérieure à 2 et donc verra son taux de perte amoindri alors qu'il peut
-#   # avoir des pratiques très mauvaises par rapport à l'agri1. Le calcul doit 
-#   # être indépendant du rdtOptimal.
-#   # il faudrait que tu calcules une note technique qui servent à ajuster le 
-#   # tauxDePertesBD. J'ai mis un exemple sous la fonction.
-#   if (get_itk_score(monTK = listAgriITKetX[[c(i,1)]])>= (tauxDePertesBD*rdtOptimal[i])) { # modifier 0.6*n[[c(i,2)]] par rdtOptimal[i]*tauxDePertesBD ? 
-#     scoreindiv[i] <- rdtOptimal[i] - (rdtOptimal[i])* (tauxDePertesBD-0.1) 
-#     print (scoreindiv[i])
-#     print ("Pratiques vertueuses") #positif
-#     a[i] <- 1
-#     print(a[i])
-#   } else {
-#     scoreindiv[i] <-rdtOptimal[i] - (rdtOptimal[i])* (tauxDePertesBD) 
-#     print (scoreindiv[i]) #négatif
-#     print ("Pratiques pas assez efficaces")
-#     a[i] <- -1
-#     print(a[i])
-#   }
-#   as <- sum(a) #sum(rep(a[i], times = length(listAgriITKetX)))
-# } 
-# 
+
 # # la meilleure note technique en utilisant toutes les pratiques : 
 # bestITKnote <- sum(itk$impactbdsurdt[itk$impactbdsurdt > 0])
 # # la note d'un agriculteur
@@ -189,27 +164,4 @@ niveauDeDifficulte <- 2 #nv1 : les agri doivent faire beaucoup de pratiques
 # rdtOptimal[i] - rdtOptimal[i]*tauxDePertesBD
 # # rdt individuel avec les pratiques
 # rdtOptimal[i] - rdtOptimal[i]*(tauxDePertesBD - tauxDePertesBD*tauxMyITKnote)
-
-
-######### ETAPE 3 : RESULTATS COLLECTIFS
-# for (i in 1:length(listAgriITKetX)) {
-#   if (as>=0) {
-#     listAgriITKetX[[c(i,2)]] <- scoreindiv[i]
-#   }else{
-#     listAgriITKetX[[c(i,2)]] <- listAgriITKetX[[c(i,2)]]
-#   }
-# }
-# listAgriITKetX
-# if (as >= 0) {
-#   print("Bravo, les pertes liés à la mouche ont baissé de 10%")  #réfléchir comment la mouche diminue d'un tour à l'autre #module de dispersion
-#   tauxDePertesBD <- tauxDePertesBD-0.1
-#   }else{
-#   print ("Mince, le taux de pertes n'a pas diminué cette saison ")
-#   }
-######### ETAPE 4 : NOUVEAUX  CHOIX DE CARTE POUR PARTIE SUIVANTE
-# listAgriITKetX[[1]][[1]] <- c("lb_nid", "lb_nid","lb_nid")
-# listAgriITKetX[[2]][[1]] <- c("dsb","lb_nid", "lb_nid","lb_nid")
-# listAgriITKetX[[3]][[1]] <- c("dsb")
-# listAgriITKetX[[4]][[1]] <- c("dsb","lb_nid", "lb_nid","lb_nid")
-######### ETAPE 5 : retour ETAPE 2
 
