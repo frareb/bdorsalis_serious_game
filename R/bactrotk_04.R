0c70cb42eba13d935ef03af54244333eef4da5bc
audngm <audrey.ngom@outlook.fr>
2023-04-12 17:07:11 +0200

    modification fct qualité et rdt

--- R/bactrotk_04.R
+++ R/bactrotk_04.R
@@ -6,39 +6,39 @@ setwd("./R")
 
 # --- 1. INITIALISATION DU JEU ------------------------------------------------
 bloc00 <- {
-niveauDeDifficulte <- 4
-tauxDePertesBD <- 0.3
-rdtOptimal <- c(40, 20, 30, 15)
-ag01 <- list(
+  niveauDeDifficulte <- 3
+  tauxDePertesBD <- 0.36
+  rdtOptimal <- c(20, 20, 20, 20)
+  ag01 <- list(
     itk = c("irr","tai","fer","dsb", "ins","pma","apg","pre" ), 
     X1_depart = rdtOptimal[1] - rdtOptimal[1]*tauxDePertesBD
-)
-ag02 <- list(
+  )
+  ag02 <- list(
     itk = c("lab","prc","irr"), 
     X2_depart = rdtOptimal[2] - rdtOptimal[2]*tauxDePertesBD
-)
-ag03 <- list(
+  )
+  ag03 <- list(
     itk = c("bio","pba","lbn","tai"), 
     X3_depart = rdtOptimal[3] - rdtOptimal[3]*tauxDePertesBD
-)
-ag04 <- list(
+  )
+  ag04 <- list(
     itk = c("lab","irr"), 
     X4_depart = rdtOptimal[4] - rdtOptimal[4]*tauxDePertesBD
-)
-listAgriITKetX <- list(ag01, ag02, ag03, ag04)
+  )
+  listAgriITKetX <- list(ag01, ag02, ag03, ag04)
 }
-listAgriITKetX #afficher points
+#afficher points de départs
+for (i in 1:length(listAgriITKetX)) { print (listAgriITKetX[[c(i,2)]])} 
 # -----------------------------------------------------------------------------
 # --- 2. CHARGEMENT DES DONNEES ET FONCTIONS ----------------------------------
 bloc01 <- {
   
 # Chargement de la liste des pratiques culturales
 itk <- read.csv(file = "testitk29.03.2023.csv", dec = ",", header = TRUE) 
-# la meilleure note technique en utilisant la moitié, le quart...des pratiques  #prise en compte de la fréquence
+# la meilleure note technique en réalisant les pratiques de luttes directes et indirectes en fonction du niveau de difficulté
 bestITKmouchenote <- sum(itk$impactmosurbd[itk$impactmosurbd > 0]*itk$Nb.de.fois.possible[itk$impactmosurbd > 0]) / niveauDeDifficulte 
-  
-#bestITKrdtnote <- sum(itk$impactmosurrdt[itk$impactmosurrdt > 0])  #prise en compte de la fréquence
-bestITKrdtnote <- sum(itk$impactmosurrdt[itk$impactmosurrdt > 0]*itk$Nb.de.fois.possible[itk$impactmosurrdt > 0]) / 2
+# la meilleure note technique en réalisant les pratiques d'optimisation de rendement
+bestITKrdtnote <- sum(itk$impactmosurrdt[itk$impactmosurrdt>0]*itk$Nb.de.fois.possible[itk$impactmosurrdt>0])
   
 # Somme des points requis des cartes jouées
 get_RN_score <- function(ressncrs){ 
@@ -49,7 +49,7 @@ get_RN_score <- function(ressncrs){
   })
   return(sum(scorei))
 }
-
+  
 # Somme de l'impact des pratiques par rapport à BD sur rendement :  si positif,
 #   choix de la pratique efficace contre prévalence mouche
 get_itkmouche_score <- function(monTK){ 
@@ -60,13 +60,14 @@ get_itkmouche_score <- function(monTK){
   })
   return(sum(scorei))
 }
-get_itkrendement_score <- function(monTK){ 
-  scorei <- sapply(seq_along(monTK), function(i){
-    score <- itk[itk$modalite.abr == monTK[i],]$impactmosurrdt
-    if(length(score) == 0){score <- NA}
-    return(score)
+# Somme de l'impact des pratiques par rapport au rendement 
+get_itkrendement_score <- function(monTK1){ 
+  rscorei <- sapply(seq_along(monTK1), function(i){
+    rscore <- itk[itk$modalite.abr == monTK1[i],]$impactmosurrdt
+    if(length(rscore) == 0){rscore <- NA}
+    return(rscore)
   })
-  return(sum(scorei))
+  return(sum(rscorei))
 }
 # Verification du nombre de points de jeu pour chaque agriculteur
 verifNumPoints <- function(listAgriITKetX){
@@ -87,25 +88,39 @@ verifNumPoints <- function(listAgriITKetX){
   return(msg)
 }
 
+#Calcul de la note lié aux pratiques de luttes
+#Si note > 1, taux de pertes va diminuer
+#Si note <1, taux de pertes va augmenter
 calculTxNoteITKmouche <- function(){
   sapply(seq_along(listAgriITKetX), function(i){
     tauxMyITKnote <- get_itkmouche_score(monTK = listAgriITKetX[[c(i,1)]])/bestITKmouchenote
   })
 }
+
+#Calcul de la note lié aux pratiques d'optimisation du rendement
 calculTxNoteITKrdt <- function(){
   sapply(seq_along(listAgriITKetX), function(i){
-    tauxMyITKnoterdt <- get_itkrendement_score(monTK = listAgriITKetX[[c(i,1)]])/bestITKrdtnote
+    tauxMyITKnoterdt <- get_itkrendement_score(monTK1 = listAgriITKetX[[c(i,1)]])/bestITKrdtnote
   })
 }
-#Calcul qualité 
+
+#Calcul de la qualité des mangues
 calculqualite <- function(){
   sapply(seq_along(listAgriITKetX), function(i){
-    qualite <- (calculTxNoteITKmouche()[i]+calculTxNoteITKrdt()[i]-tauxDePertesBD)*2 #*2 pour prendre en compte décimal et avoir score plus haut
-    #changer formule ou faire un if car si un joueur investi bcp dans rdt mais pas mouche, 
-    # il aura une meilleur qualité que ceux qui investissent dans les deux
+    if (tauxDePertesBD>0.20) {
+      if (calculTxNoteITKmouche()[i]>0 & calculTxNoteITKmouche()[i]<2 ) {
+        qualite <- abs(1-(tauxDePertesBD+tauxDePertesBD* (1-calculTxNoteITKmouche()[i])))*10
+      }else if (calculTxNoteITKmouche()[i]<=0) {
+        qualite <- (1-0.9)*10   
+      }else if (calculTxNoteITKmouche()[i]>=2) {
+        qualite <- 10
+      }
+    }else{
+      qualite <- 8 #à modifier  
+    }
   })
 }
-# calcul du rdt avec le taux de note ITK et incidence BD
+# Calcul nouveau taux de perte et rdt avec le taux de note ITK et incidence BD
 calculRdt <- function(){
   if( sum(calculTxNoteITKmouche()>=1)>=(0.5*(length(listAgriITKetX)))){ 
     newTauxDePertesBD <- tauxDePertesBD - tauxDePertesBD*0.1 
@@ -121,43 +136,37 @@ calculRdt <- function(){
   tauxDePertesBD <- newTauxDePertesBD
   if(tauxDePertesBD > 1){tauxDePertesBD <- 1}
   rdtReel <- sapply(seq_along(listAgriITKetX), function(i){
-    if (tauxDePertesBD > 0.15) { #prend en compte le nouveau taux de perte
-      if (calculTxNoteITKmouche()[i]<0.5) {
-        rdtReel <-rdtOptimal[i] -  rdtOptimal[i]*(tauxDePertesBD+0.15) 
-      }else if(calculTxNoteITKmouche()[i]>=1){ 
-        rdtReel <-rdtOptimal[i] -  rdtOptimal[i]*(tauxDePertesBD-0.15)
-      }else{ 
-        rdtReel <-rdtOptimal[i] -  rdtOptimal[i]*(tauxDePertesBD+0.10)
-      } 
-      if (calculTxNoteITKrdt()[i]>=1) {
-        rdtReel <-rdtReel +  3
-      } 
-    }else{
-      if(calculTxNoteITKrdt()[i]>=1) {
-        rdtReel<- rdtOptimal[i]
-      }else if (calculTxNoteITKrdt()[i]<0.7) {
-        rdtReel<- rdtOptimal[i]*0.75
+    if (tauxDePertesBD>0.20){
+      if (tauxDePertesBD>0.8){
+        rdtReel <- 0.5*rdtOptimal[i] + ((0.5*rdtOptimal[i])* calculTxNoteITKrdt()[i])-(0.5*rdtOptimal[i])*0.95
+      }else if(calculTxNoteITKrdt()[i]<=0){
+        rdtReel <- 0.5*rdtOptimal[i] - (0.5*rdtOptimal[i])*(tauxDePertesBD+tauxDePertesBD* (1-calculTxNoteITKmouche()[i]))
+      }else if(calculTxNoteITKmouche()[i]<=0){
+        rdtReel <- 0.5*rdtOptimal[i] + ((0.5*rdtOptimal[i])* calculTxNoteITKrdt()[i])-(0.5*rdtOptimal[i])*0.95
       }else{
-        rdtReel <- rdtOptimal[i]*calculTxNoteITKrdt()[i]
-      }  
-    }
-    if (rdtReel > rdtOptimal [i]){rdtReel <- rdtOptimal[i]} #si rendement final dépasse, donner rendement final #à supp pour calibrer?
-    msg <- paste0(
-      "Agri", i, 
-      ": rdt optimal: ", round(rdtOptimal[i], digits = 2),
-      " ; rdt début : ", round(listAgriITKetX[[c(i,2)]], digits = 2),
-      " ; rdt réel: ", round(rdtReel, digits = 2),
-      " ; note ITK rdt: ", round(calculTxNoteITKrdt()[i], digits = 2), 
-      " ; note ITK mouche: ", round(calculTxNoteITKmouche()[i], digits = 2), 
-      " ; qualité: ",round(calculqualite()[i]),
-      "\n"
-    )
-    cat(msg)
-    return(rdtReel)
+        rdtReel <- 0.5*rdtOptimal[i] + ((0.5*rdtOptimal[i])* calculTxNoteITKrdt()[i])-(0.5*rdtOptimal[i])*(tauxDePertesBD+tauxDePertesBD* (1-calculTxNoteITKmouche()[i]))
+      }
+   }else{
+        rdtReel <- 0.5*rdtOptimal[i] + ((0.5*rdtOptimal[i])* calculTxNoteITKrdt()[i])
+      }
+   if (rdtReel > rdtOptimal [i]){rdtReel <- rdtOptimal[i]} 
+   if (rdtReel <0) {rdtReel <- 0} 
+   msg <- paste0(
+        "Agri", i, 
+        ": rdt optimal: ", round(rdtOptimal[i], digits = 2),
+        " ; rdt début : ", round(listAgriITKetX[[c(i,2)]], digits = 2),
+        " ; rdt réel: ", round(rdtReel, digits = 2),
+        " ; note ITK rdt: ", round(calculTxNoteITKrdt()[i], digits = 2), 
+        " ; note ITK mouche: ", round(calculTxNoteITKmouche()[i], digits = 2), 
+        " ; qualité: ",round(calculqualite()[i]),
+        "\n"
+      )
+   cat(msg)
+   return(rdtReel)
   })
-  return(list(tauxDePertesBD, rdtReel))
-}
-
+    return(list(tauxDePertesBD, rdtReel))
+  }
+  
 }
 # --- 3. BOUCLE DU JEU --------------------------------------------------------
 listAgriITKetX #afficher points
@@ -168,10 +177,10 @@ listAgriITKetX <- lapply(seq_along(listAgriITKetX), function(i){
   listAgriITKetX[[i]][[2]] <- tourDeJeu[[2]][i]
   return(listAgriITKetX[[i]])
 })
-listAgriITKetX[[1]][[1]] <- c("irr","tai","fer","dsb", "ins","pma","apg","pre","tai","dsb" )
-listAgriITKetX[[2]][[1]] <- c("lab","prc","irr" ,"fer","dsb")
-listAgriITKetX[[3]][[1]] <- c("apg","tai","pre","tai","lbn","apg","dsb")
-listAgriITKetX[[4]][[1]] <- c("pre","fer","tai","irr")
+listAgriITKetX[[1]][[1]] <- c("lbe","tai","pre","apg","fer")
+listAgriITKetX[[2]][[1]] <- c("fer","dsb","lbn","tai","dsb", "ins","pma","apg","pre","tai")
+listAgriITKetX[[3]][[1]] <- c("apg","tai","pre","apg","fer","tai","dsb")
+listAgriITKetX[[4]][[1]] <- c("tai","fer","dsb","lbn","tai","fer","dsb","pre","fer","apg","pre")
 # --- 4. CHANGEMENTS ANNEXES AU COURS DU JEU --------------------------------------------------------
 niveauDeDifficulte <- 2 #nv1 : les agri doivent faire beaucoup de pratiques
 #nv2 : les agri doivent au moins faire la moitié des pratiques
